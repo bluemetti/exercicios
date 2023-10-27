00eef99e3e4a0893ebba08b16f222f3efd4b0d6b
bluemetti <143762698+bluemetti@users.noreply.github.com>
2023-10-27 10:24:47 -0300

    hahahahaha

--- ntc.c
+++ ntc.c
@@ -13,9 +13,19 @@ int main(){
     if(!arq) return 0;
 
     printf("\nMensagem 1:\n");
-    scanf("%s", msg);
-    fprintf(arq, strcat(msg, "\n"));
+    fprintf(arq, "3.0 3.2 4.5 \n7.2 9.5 2.2");
     fclose(arq);
-
+    arq = fopen(nome, "r");
+    float n1, n2, n3, n4;
+    while(fscanf(arq, "%f %f %f", &n1, &n2, &n3)!= EOF){
+        printf("nota 1: %.1f    nota 2: %.1f   nota 3: %.1f\n", n1, n2, n3);
+    }
+    /*
+    fscanf(arq, "%f %f %f %f", &n1, &n2, &n3, &n4);
+    printf("n1 == %.1f, n2 == %.1f, n3 == %.1f, n4==%.1f", n1, n2, n3, n4);
+    */
+    //para remover uma nota especifica->->
+    
+    fclose(arq);
     return 1;
 }
